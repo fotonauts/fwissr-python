d8a1d1b32d5c4c200c0cd1a7ff4647bfd3f5b816
Pierre Baillet <oct@fotonauts.com>
2013-10-29 14:07:20 +0100

    More tests and WIP initial API

--- fwissr/fwissr.py
+++ fwissr/fwissr.py
@@ -1,12 +1,83 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 
-# default path where main conf file is located
-DEFAULT_MAIN_CONF_PATH = "/etc/fwissr"
 
-# default directory (relative to current user's home) where user's main conf file is located
-DEFAULT_MAIN_USER_CONF_DIR = ".fwissr"
+class FwissrModule(object):
+    # main conf file
+    MAIN_CONF_FILE = "fwissr.json"
+    # default path where main conf file is located
+    DEFAULT_MAIN_CONF_PATH = "/etc/fwissr"
+    # default directory (relative to current user's home) where user's main conf file is located
+    DEFAULT_MAIN_USER_CONF_DIR = ".fwissr"
 
-# main conf file
-MAIN_CONF_FILE = "fwissr.json"
+    def __init__(self):
+        self._main_conf_path = self.DEFAULT_MAIN_CONF_PATH
+        self._main_user_conf_path = os.path.join(self.find_home(), self.DEFAULT_MAIN_USER_CONF_DIR)
+
+    def main_conf_path():
+        doc = "The main_conf_path property."
+        def fget(self):
+            return self._main_conf_path
+        def fset(self, value):
+            self._main_conf_path = value
+        return locals()
+    main_conf_path = property(**main_conf_path())
+
+
+    def main_user_conf_path():
+        doc = "The main_user_conf_path property."
+        def fget(self):
+            return self._main_user_conf_path
+        def fset(self, value):
+            self._main_user_conf_path = value
+        return locals()
+    main_user_conf_path = property(**main_user_conf_path())
+
+    def find_home(self):
+        for v in ('HOME', 'USERPROFILE'):
+            if v in os.environ:
+                return v
+
+        if "HOMEDRIVE" in os.environ and "HOMEPATH" in os.environ:
+            return "%s:%s" % (os.environ["HOMEDRIVE"], os.environ["HOMEPATH"])
+
+        try:
+            return os.path.expanduser("~")
+        except Exception:
+            if os.sep == '\\':
+                return "C:/"
+            else:
+                return "/"
+
+    def parse_args(self, argv):
+        pass
+
+
+    def global_registry():
+        """Global Registry
+
+
+        NOTE: Parses main conf files (/etc/fwissr/fwissr.json and ~/.fwissr/fwissr.json) then uses 'fwissr_sources' setting to setup additional sources
+
+        Example of /etc/fwissr/fwissr.json file:
+
+         {
+           'fwissr_sources': [
+             { 'filepath': '/mnt/my_app/conf/' },
+             { 'filepath': '/etc/my_app.json' },
+             { 'mongodb': 'mongodb://db1.example.net/my_app', 'collection': 'config', 'refresh': true },
+           ],
+           'fwissr_refresh_period': 30,
+        }
+        access global registry with Fwissr['/foo/bar']
+        """
+        pass
+
+    def get(self, key):
+        return self[key]
+
+    def __getitem__(self, key):
+        return "plop"
+
--- tests/test_fwissr.py
+++ tests/test_fwissr.py
@@ -9,140 +9,24 @@ Tests for `fwissr` module.
 """
 
 import unittest
-
-from fwissr import fwissr
-
-from pymongo import MongoClient
-import os
-import json
-import yaml
-import shutil
-import locale
-
-def setup_global_conf():
-    # create additional file sources
-    first = 'mouarf.lol.json'
-    second = 'trop.mdr.json'
-
-
-    create_tmp_conf_file(first, {
-        'meu': 'ringue',
-        'pa': { 'pri': 'ka'},
-    })
-
-    create_tmp_conf_file(second, {
-        'gein': 'gembre',
-        'pa': { 'ta': 'teu'},
-    })
-
-    # create additional mongodb sources
-    create_tmp_mongo_col('roque.fort', {
-        'bar': 'baz',
-    })
-
-    create_tmp_mongo_col('cam.en.bert', {
-        'pim': { 'pam': [ 'pom', 'pum' ] },
-    })
-
-    # create main conf file
-    fwissr_conf = {
-        'fwissr_sources': [
-            { 'filepath': tmp_conf_file(first) },
-            { 'filepath': tmp_conf_file(second), 'top_level': True },
-            { 'mongodb': mongo_connection_url(tmp_mongo_db()), 'collection': 'roque.fort', 'top_level': True },
-            { 'mongodb': mongo_connection_url(tmp_mongo_db()), 'collection': 'cam.en.bert' },
-        ],
-        'fwissr_refresh_period': 5,
-        'foo':'bar',
-    }
-    create_tmp_conf_file('fwissr.json', fwissr_conf)
-
-
-
-def tmp_conf_dir():
-    return "/tmp/fwissr.spec"
-
-def tmp_conf_file(filename):
-    return "%s/%s" % (tmp_conf_dir(), filename)
-
-def create_tmp_conf_file(filename, conf):
-    conf_file_path = os.path.join(tmp_conf_dir(), filename)
-
-    if os.path.exists(conf_file_path):
-        os.unlink(conf_file_path)
-
-    if not os.path.lexists(tmp_conf_dir()):
-        os.makedirs(tmp_conf_dir())
-
-    f = open(conf_file_path, 'w')
-    if os.path.splitext(filename)[1] == ".json":
-        f.write(json.dumps(conf, sort_keys=True))
-    elif os.path.splitext(filename)[1] == ".yml":
-        f.write(yaml.dump(conf, Dumper=yaml.CDumper))
-    else:
-        raise Exception("Unsupported conf file type", filename)
-    f.close()
-
-def delete_tmp_conf_file():
-    if os.path.abspath(tmp_conf_dir()) == os.path.abspath(fwissr.DEFAULT_MAIN_CONF_PATH):
-        raise Exception("Hey, don't delete all legal conf files !", tmp_conf_dir())
-    shutil.rmtree(tmp_conf_dir(), True)
-
-def set_tmp_conf(conf_dir = tmp_conf_dir, user_conf_dir = ""):
-    Fwissr.conf_dir = conf_dir
-    Fwissr.user_conf_dir = user_conf_dir
-
-
-def env_or(env_name, or_value):
-    """Returns the env variable value or or_value if unset"""
-    if env_name in os.environ:
-        return os.environ[env_name]
-    else:
-        return or_value
-
-def tmp_mongo_hostname():
-    return env_or("MONGO_HOSTNAME", "localhost")
-
-def tmp_mongo_port():
-    return env_or("MONGO_PORT", 27017)
-    if "MONGO_PORT" in os.environ:
-        return locale.atoi(os.environ["MONGO_PORT"])
-    else:
-        return 27017
-
-def tmp_mongo_db():
-    return "fwissr_spec"
-
-def mongo_connection_url(db = ""):
-    return "mongodb://%s:%s/%s" % (tmp_mongo_hostname(),
-        tmp_mongo_port(),
-        db)
-
-def mongo_connection():
-    return MongoClient(mongo_connection_url())
-
-def create_tmp_mongo_col(name, conf):
-    col = mongo_connection()[tmp_mongo_db()].create_collection(name)
-    for key, val in conf.iteritems():
-        col.insert({'_id': key, 'value' : val})
-
-def delete_tmp_mongo_db():
-    client = mongo_connection()
-    client.drop_database(tmp_mongo_db())
-
+from test_helpers import *
+import fwissr
+import Fwissr
 
 class TestFwissr(unittest.TestCase):
 
+    @classmethod
+    def setupClass(cls):
+        fwissr.main_conf_path = tmp_conf_dir()
+
     def setUp(self):
-        delete_tmp_mongo_db()
-        setup_global_conf()
-
-    def test_something(self):
-        pass
-
-    def tearDown(self):
         delete_tmp_conf_file()
         delete_tmp_mongo_db()
+
+    def test_manager_global_registry(self):
+        setup_global_conf()
+        self.assertEqual(Fwissr['/foo'], 'bar')
+
 
 if __name__ == '__main__':
     unittest.main()
